444d95e97f20e741a3704082377f331185c1ecec
Ann-Marie <anniedillier@gmail.com>
2020-08-09 01:43:00 -0700

    add input check

--- assets/script.js
+++ assets/script.js
@@ -3,37 +3,60 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 var writePassword = function () {
-  var password = generatePassword()
+  var options = getOptions()
+  //var password = generatePassword()
   //password
 }
 
-var generatePassword = function () {
+var getOptions = function () {
     var promptLength = window.prompt("Select a lenght between 8 and 128?");
     while (promptLength <= 7 || promptLength >= 129 ) {
+      window.alert("You need to provide a valid answer! Please try again.");
       var promptLength = window.prompt("Select a lenght between 8 and 128?");
     } 
     length = promptLength
     console.log (length)
 
-
   var promptLower = window.prompt("Do you want to include lowercase letters?");
-  
+    while (promptLower === "" || promptLower === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      var promptLower = window.prompt("Do you want to include lowercase letters?");
+    if (promptLower === "yes" || promptLower === "YES") {
+      randomLower()
+      console.log(randomLower)
+    }
+  }
 
   var promptUpper = window.prompt("Do you want to include uppercase letters?");
-  if (promptUpper === "y" || promptUpper === "Y") {
-    randomUpper()
-    console.log (randomUpper);
+    while (promptUpper === "" || promptUpper === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      var promptUpper = window.prompt("Do you want to include uppercase letters?");
+    if (promptUpper === "y" || promptUpper === "Y") {
+      randomUpper()
+      console.log (randomUpper);
+    }
   }
 
   var promptNum = window.prompt("Do you want to include numbers?"); 
-  if (promptLower === "y" || promptLower === "Y") {
-    randomNum()
-    console.log (randomNum);
+    while (promptNum === "" || promptNum === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      var promptNum = window.prompt("Do you want to include numbers?");
+    if (promptNum === "y" || ppromptNum === "Y") {
+      randomNum()
+      console.log (randomNum);
+    }
   }
 
   var promptSpecial = window.prompt("Do you want to include special characters?");
-
-};
+    while (promptSpecial === "" || promptSpecial === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      var promptSpecial = window.prompt("Do you want to include special characters?");
+    if (promptSpecial === "y" || promptSpecial === "Y") {
+      randomSpecial()
+      console.log (randomSpecial);
+    }
+  }
+}; // end function
 
  // var passwordText = document.querySelector("#password");
 
@@ -43,7 +66,7 @@ var generatePassword = function () {
 
 //lower
 var randomLower = function () {
-  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  var lower = 'abcdefghijklmnopqrstuvwxyz';
   return lower[Math.floor(Math.random() * upper.length)];
 } // end randomLower function
 
