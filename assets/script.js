c1010550c5a75a53cd8ff92882857bd4a43e9464
Ann-Marie <anniedillier@gmail.com>
2020-08-08 22:52:47 -0700

    prompt for options

--- assets/script.js
+++ assets/script.js
@@ -2,41 +2,74 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+var writePassword = function () {
+  var password = generatePassword()
+  //password
 }
 
-// gather options
-  //length
-  //charters
-  var Length = window.prompt("length greater than 8");
-               
-  
+var generatePassword = function () {
+    var promptLength = window.prompt("Select a lenght between 8 and 128?");
+    while (promptLength <= 7 || promptLength >= 129 ) {
+      var promptLength = window.prompt("Select a lenght between 8 and 128?");
+    } 
+    length = promptLength
+    console.log (length)
 
 
-// genertate password
-  // check options
-  switch (options) {
-    case "Lenght":
-    options.lenght();
-    break;
+  var promptLower = window.prompt("Do you want to include lowercase letters?");
   
-  case "upperCase":
-    options.upperCase();
-    break;
-    
-  case "lowerCase":
-    options.upperCase();
-    break;
-    
+
+  var promptUpper = window.prompt("Do you want to include uppercase letters?");
+  if (promptUpper === "y" || promptUpper === "Y") {
+    randomUpper()
+    console.log (randomUpper);
   }
 
-// print password to screen
+  var promptNum = window.prompt("Do you want to include numbers?"); 
+  if (promptLower === "y" || promptLower === "Y") {
+    randomNum()
+    console.log (randomNum);
+  }
 
+  var promptSpecial = window.prompt("Do you want to include special characters?");
+
+};
+
+ // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
+
+
+
+//lower
+var randomLower = function () {
+  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  return lower[Math.floor(Math.random() * upper.length)];
+} // end randomLower function
+
+
+//upper
+function randomUpper() {
+  var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return upper[Math.floor(Math.random() * upper.length)];
+} // end randomUpperfunction
+
+//numbers
+var randomNum = function() {
+  var value = Math.floor(Math.random() * 10);
+  return value;
+};
+
+//Special charters
+function randomSpecial() {
+const symbols = '!@#$%^&*()+-./:;<=>?[\]^_`{|}~ '[math.floor(math.random() * symbols.length)]; 
+return sysmbols
+} // end randomSpecial function
+
+
+/*          
+print password to screen
+*/
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
